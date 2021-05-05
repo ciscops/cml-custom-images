5f256f3a022ba66b365c26323e9901750a0407b8
jasonking3 <jk1ng@yahoo.com>
2021-05-05 11:13:01 -0700

    Enable https with self-signed cert

--- packer_gitlab/files/gitlab-config.sh
+++ packer_gitlab/files/gitlab-config.sh
@@ -3,7 +3,8 @@
 ipaddr=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 
 if [ $ipaddr ]; then
-sed -i "s/^external_url.*/external_url \"http:\/\/$ipaddr\"/g" /etc/gitlab/gitlab.rb
+sed -i "s/^external_url.*/external_url \"https:\/\/$ipaddr\"/g" /etc/gitlab/gitlab.rb
+sed -i "s/^# letsencrypt\['enable'\].*/letsencrypt\['enable'\] = false/g" /etc/gitlab/gitlab.rb
 /usr/bin/gitlab-ctl reconfigure
 else
 echo "Failed to obtain primary IP address"
