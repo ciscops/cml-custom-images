abdc1b4024f1ec1f4bb24eaab29d2f43a46d0154
jasonking3 <jk1ng@yahoo.com>
2021-05-06 18:35:07 +0000

    General improvements

--- packer_gitlab/files/gitlab-config.sh
+++ packer_gitlab/files/gitlab-config.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
-ipaddr=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+IPADDR=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+PASSWORD=$(sed -n "s/^gitlab_rails\['initial_root_password'\] = \"\(.*\)\"/\1/p" /etc/gitlab/gitlab.rb)
+TOKEN=$(sed -n "s/^gitlab_rails\['initial_shared_runners_registration_token'\] = \"\(.*\)\"/\1/p" /etc/gitlab/gitlab.rb)
 
-if [ $ipaddr ]; then
-sed -i "s/^external_url.*/external_url \"http:\/\/$ipaddr:8081\"/g" /etc/gitlab/gitlab.rb
-sed -i "s/^# letsencrypt\['enable'\].*/letsencrypt\['enable'\] = false/g" /etc/gitlab/gitlab.rb
+# If we have a password and an address, update the MOTD
+if [[ ! -z $PASSWORD && ! -z $IPADDR ]]; then
+/bin/cat > /etc/motd <<EOF
+###################
+GitLab is accessible at http://$IPADDR:8081 with credentials root/$PASSWORD
+###################
+
+EOF
+fi
+
+if [[ ! -z $IPADDR  ]]; then
+# Update GitLab external_url with dynamic IP address and reconfigure
+sed -i "s/^external_url.*/external_url \"http:\/\/$IPADDR:8081\"/g" /etc/gitlab/gitlab.rb
 /usr/bin/gitlab-ctl reconfigure
-gitlab-runner register --non-interactive --url http://$ipaddr:8081 registration-token zkguBxeyBzsEioz-KzKb --executor docker --docker-image python:2.7
+
+# Register the runner
+RUNNERS=$(gitlab-runner list 2>&1 | grep Token)
+if [[ ! $RUNNERS && $TOKEN ]]; then
+gitlab-runner register --non-interactive --url http://$IPADDR:8081 --registration-token "$TOKEN" --executor docker --docker-image python:2.7
+fi
 else
 echo "Failed to obtain primary IP address"
+exit 1
 fi
--- packer_gitlab/scripts/installMain.sh
+++ packer_gitlab/scripts/installMain.sh
@@ -2,27 +2,34 @@
 
 printf "==> Installing GitLab\n"
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
 
 # Install GitLab
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash   
-sudo EXTERNAL_URL="http://gitlab.example.com" apt-get install gitlab-ce
+apt-get -qq -y install gitlab-ce
+
+# Configure GitLab
+# sed -i "s/^# letsencrypt\['enable'\].*/letsencrypt\['enable'\] = false/g" /etc/gitlab/gitlab.rb
+sed -i "s/^# gitlab_rails\['initial_root_password'\].*/gitlab_rails\['initial_root_password'\] = \"$GITLAB_ROOT_PASSWORD\"/g" /etc/gitlab/gitlab.rb
+sed -i "s/^# gitlab_rails\['initial_shared_runners_registration_token'\].*/gitlab_rails\['initial_shared_runners_registration_token'\] = \"$GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN\"/g" /etc/gitlab/gitlab.rb
 
 # Install Runner
 curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
-sudo dpkg -i gitlab-runner_amd64.deb
+dpkg -i gitlab-runner_amd64.deb
 
 # Create systemd service
-sudo mv /tmp/gitlab-config.sh /usr/local/sbin/gitlab-config.sh
-sudo mv /tmp/gitlab-config.service /etc/systemd/system/gitlab-config.service
+mv /tmp/gitlab-config.sh /usr/local/sbin/gitlab-config.sh
+mv /tmp/gitlab-config.service /etc/systemd/system/gitlab-config.service
 # systemctl start gitlab-config
 systemctl enable gitlab-config
 
 # Install Runner
 curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
-sudo dpkg -i gitlab-runner_amd64.deb
+dpkg -i gitlab-runner_amd64.deb
 
 # An MOTD to hint at what to do once folks log in
 printf "==> Generating MOTD\n"
